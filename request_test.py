807cc3bdb71995ef0efc853cac21ee899b81216d
Alexandre Mota <alexandremot@gmail.com>
2021-02-14 18:12:11 -0300

    melhorias no codigo

--- request_test.py
+++ request_test.py
@@ -2,7 +2,7 @@
 import requests
 
 url = "http://18.231.152.211:5000/pedidos"
-payload=""
+payload = ""
 headers = {'Content-Type': 'application/json'}
 response = requests.request("GET", url, headers=headers, data=payload)
 print(response.text)
--- socket_connection_test.py
+++ socket_connection_test.py
@@ -1,20 +1,20 @@
 
 import socket
 
+
 def port_test():
-	s = socket.socket()
-	address = '18.231.152.211'
-	port = 5000  # port number is a number, not string
-	try:
-		s.connect((address, port)) 
-		# originally, it was 
-		# except Exception, e: 
-		# but this syntax is not supported anymore. 
-		print(s)
-	except Exception as e: 
-		print("something's wrong with %s:%d. Exception is %s" % (address, port, e))
-	finally:
-		s.close()
+    s = socket.socket()
+    address = '18.231.152.211'
+    port = 5000  # port number is a number, not string
+    try:
+        s.connect((address, port))
+        print(s)
+    except Exception as e:
+        print("something's wrong with %s:%d.")
+        print("Exception is %s" % (address, port, e))
+    finally:
+        s.close()
+
 
 if __name__ == "__main__":
     port_test()
